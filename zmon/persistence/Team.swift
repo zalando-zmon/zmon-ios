1e5338c5a31f1f0a9a0243a281045d22547a899a
Ahmed Shaalan <ahmed@i25.me>
2016-04-21 16:03:20 +0200

    Implement dashboard saving to disk between sessions

--- zmon/persistence/Team.swift
+++ zmon/persistence/Team.swift
@@ -8,10 +8,25 @@
 
 import Foundation
 
-class Team: NSObject {
+class Team: NSObject, NSCoding {
     
-    //Temporary storage
-    private static var teamList: [Team] = []
+    //MARK: Persistence names
+    static let teamListPropertyName = "zmon.team.teamList"
+    static let namePropertyName = "zmon.team.name"
+    static let observedPropertyName = "zmon.team.observed"
+    
+    private static var teamList: [Team] = {
+        //If teamList was previously persisted on disk, fetch it, otherwise return an empty array
+        let defaults = NSUserDefaults.standardUserDefaults()
+        let teamListData = defaults.objectForKey(Team.teamListPropertyName)
+        if teamListData != nil {
+            let teamList = NSKeyedUnarchiver.unarchiveObjectWithData(teamListData as! NSData)
+            return teamList as!  [Team]
+        }
+        
+        return []
+        
+    }()
     
     //MARK: Properties
     var name: String
@@ -22,6 +37,23 @@ class Team: NSObject {
         self.name = name
         self.observed = observed
     }
+    
+    //MARK: NSCoding
+    required convenience init?(coder decoder: NSCoder) {
+        let observed = decoder.decodeBoolForKey(Team.observedPropertyName)
+        guard let name = decoder.decodeObjectForKey(Team.namePropertyName) as? String
+            else {
+                return nil;
+        }
+        self.init(name: name, observed: observed)
+    }
+    
+    
+    func encodeWithCoder(coder: NSCoder) {
+        coder.encodeObject(self.name, forKey:Team.namePropertyName)
+        coder.encodeBool(self.observed, forKey: Team.observedPropertyName)
+    }
+    
     
     func save() {
         let idx = Team.teamList.indexOf { (team: Team) -> Bool in
@@ -35,6 +67,15 @@ class Team: NSObject {
             Team.teamList.removeAtIndex(idx!)
             Team.teamList.append(self)
         }
+        
+        self.persistObservedTeams()
+    }
+    
+    func persistObservedTeams() {
+        let defaults = NSUserDefaults.standardUserDefaults()
+        let data = NSKeyedArchiver.archivedDataWithRootObject(Team.teamList)
+        
+        defaults.setObject(data, forKey: Team.teamListPropertyName);
     }
     
     static func findByName(name name: String) -> Team? {
