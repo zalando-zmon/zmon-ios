1f7702fde7a57011fb5e150b2d26d354dbd008c6
Wojciech Czerski <wojciech.lukasz.czerski@zalando.de>
2016-04-26 09:44:08 +0200

    Implement subscribe / unsubscribe logic for remote alerts§

--- zmon/controllers/RemoteAlertsVC.swift
+++ zmon/controllers/RemoteAlertsVC.swift
@@ -14,6 +14,7 @@ class RemoteAlertsVC: BaseVC {
     
     let alertService = ZmonAlertsService()
     var remoteAlerts: [ZmonServiceResponse.Alert] = []
+    var observedAlerts: [ZmonServiceResponse.Alert] = []
     
     override func viewDidLoad() {
         
@@ -31,9 +32,9 @@ class RemoteAlertsVC: BaseVC {
     }
     
     func fetchRemoteAlerts() {
-        
+
         alertService.listRemoteObservableAlertsWithCompletion { alerts in
-            
+
             if let alerts = alerts {
                 
                 log.info("Successfully fetched \(alerts.count) alerts!")
@@ -45,6 +46,53 @@ class RemoteAlertsVC: BaseVC {
                 log.error("Failed to fetch remote alerts")
             }
         }
+    }
+    
+    func subscribeToAlert(alert: ZmonServiceResponse.Alert, indexPath: NSIndexPath) {
+        
+        alertService.subscribeToAlertWithID("\(alert.id)", success: {
+            
+            self.observedAlerts.append(alert)
+            self.tableView.reloadRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+            
+        }) { (error) in
+            
+        }
+    }
+    
+    func unsubscribeFromAlert(alert: ZmonServiceResponse.Alert, indexPath: NSIndexPath) {
+        
+        alertService.unsubscribeFromAlertWithID("\(alert.id)", success: {
+            
+            self.removeObservedAlert(alert)
+            self.tableView.reloadRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+            
+        }) { (error) in
+            
+        }
+    }
+
+    func isAlertObserved(alert: ZmonServiceResponse.Alert) -> Bool {
+        
+        let index = observedAlerts.indexOf { (observedAlert) -> Bool in
+            return alert.id == observedAlert.id
+        }
+        
+        return index != nil
+    }
+    
+    func removeObservedAlert(alert: ZmonServiceResponse.Alert) {
+        
+        let index = observedAlerts.indexOf { (alert) -> Bool in
+            return alert.id == alert.id
+        }
+        
+        guard let existingIndex = index else {
+            log.warning("Could not remove observed alert - the alert with id = \(alert.id) does not exist")
+            return
+        }
+        
+        observedAlerts.removeAtIndex(existingIndex)
     }
 }
 
@@ -63,10 +111,25 @@ extension RemoteAlertsVC: UITableViewDataSource {
         let cell = tableView.dequeueReusableCellWithIdentifier("AlertCell", forIndexPath: indexPath)
         let alert = remoteAlerts[indexPath.row];
         
-        cell.accessoryType = .None
+        cell.accessoryType = isAlertObserved(alert) ? .Checkmark : .None
         cell.textLabel?.text = alert.name
         cell.detailTextLabel?.text = "\(alert.id), \(alert.team)"
         
         return cell
+    }
+}
+
+extension RemoteAlertsVC: UITableViewDelegate {
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        let alert = remoteAlerts[indexPath.row];
+        
+        if (isAlertObserved(alert)) {
+            unsubscribeFromAlert(alert, indexPath: indexPath)
+            
+        } else {
+            subscribeToAlert(alert, indexPath: indexPath)
+        }
     }
 }
