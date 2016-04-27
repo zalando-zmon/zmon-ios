447ba0cf4511eb211c68977bece761b8ca13cec4
Wojciech Czerski <wojciech.lukasz.czerski@zalando.de>
2016-04-27 09:45:40 +0200

    Implement remote alerts searching

--- zmon/controllers/RemoteAlertsVC.swift
+++ zmon/controllers/RemoteAlertsVC.swift
@@ -12,8 +12,10 @@ class RemoteAlertsVC: BaseVC {
     
     @IBOutlet weak var tableView: UITableView!
     
+    let searchController = UISearchController(searchResultsController: nil)
     let alertService = ZmonAlertsService()
     var remoteAlerts: [ZmonServiceResponse.Alert] = []
+    var filteredRemoteAlerts: [ZmonServiceResponse.Alert] = []
     var observedAlerts: [ZmonServiceResponse.Alert] = []
     
     var completionCallback: ((observedAlerts: [ZmonServiceResponse.Alert]) -> ())?
@@ -21,11 +23,13 @@ class RemoteAlertsVC: BaseVC {
     override func viewDidLoad() {
         
         super.viewDidLoad()
-        
+
         tableView.estimatedRowHeight = 40
         tableView.rowHeight = UITableViewAutomaticDimension
         
         navigationItem.title = "RemoteAlerts".localized
+        
+        setupSearch()
     }
 
     @IBAction func onDoneButtonTap(sender: UIBarButtonItem) {
@@ -95,6 +99,32 @@ class RemoteAlertsVC: BaseVC {
         
         observedAlerts.removeAtIndex(existingIndex)
     }
+    
+    func setupSearch() {
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.barStyle = .Black
+        searchController.dimsBackgroundDuringPresentation = false
+        definesPresentationContext = true
+        tableView.tableHeaderView = searchController.searchBar
+    }
+    
+    func isSearching() -> Bool {
+        return searchController.active && searchController.searchBar.text != ""
+    }
+
+    func filterRemoteAlertsForSearchText(searchText: String, scope: String = "All") {
+
+        filteredRemoteAlerts = remoteAlerts.filter { alert in
+            
+            let nameMatches = alert.name.lowercaseString.containsString(searchText.lowercaseString)
+            let teamMatches = alert.team.lowercaseString.containsString(searchText.lowercaseString)
+            let idMatches = String(alert.id).hasPrefix(searchText)
+            
+            return nameMatches || teamMatches || idMatches
+        }
+        
+        tableView.reloadData()
+    }
 }
 
 extension RemoteAlertsVC: UITableViewDataSource {
@@ -104,13 +134,13 @@ extension RemoteAlertsVC: UITableViewDataSource {
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return remoteAlerts.count
+        return isSearching() ? filteredRemoteAlerts.count : remoteAlerts.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCellWithIdentifier("AlertCell", forIndexPath: indexPath)
-        let alert = remoteAlerts[indexPath.row];
+        let cell = self.tableView.dequeueReusableCellWithIdentifier("AlertCell", forIndexPath: indexPath)
+        let alert = isSearching() ? filteredRemoteAlerts[indexPath.row] : remoteAlerts[indexPath.row];
         
         cell.accessoryType = isAlertObserved(alert) ? .Checkmark : .None
         cell.textLabel?.text = alert.name
@@ -124,13 +154,23 @@ extension RemoteAlertsVC: UITableViewDelegate {
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         
-        let alert = remoteAlerts[indexPath.row];
+        let alert = isSearching() ? filteredRemoteAlerts[indexPath.row] : remoteAlerts[indexPath.row];
         
         if (isAlertObserved(alert)) {
             unsubscribeFromAlert(alert, indexPath: indexPath)
             
         } else {
             subscribeToAlert(alert, indexPath: indexPath)
+        }
+    }
+}
+
+extension RemoteAlertsVC: UISearchResultsUpdating {
+
+    func updateSearchResultsForSearchController(searchController: UISearchController) {
+        
+        if let searchText = searchController.searchBar.text {
+            filterRemoteAlertsForSearchText(searchText)
         }
     }
 }
